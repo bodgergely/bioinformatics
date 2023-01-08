34d5bff51bd8d9dded7d551e492a254678b1827f
Greg Bod <gergely.bod@hp.com>
2023-01-08 18:08:59 +0100

    new code in python

--- python/genome.py
+++ python/genome.py
@@ -2,13 +2,27 @@ import fileinput
 import sys
 import array
 from collections import defaultdict
-from typing import List, Set, Dict
+from typing import List, Set, Dict, Iterable, Tuple
 
 """
 k: kmer
 """
 
-def pattern_count(text, pattern):
+GENOME_TABLE = ['A', 'T', 'C', 'G']
+
+GENOME_COMPLEMENT_TABLE = {
+    'A':'T',
+    'T':'A',
+    'C':'G',
+    'G':'C',
+}
+
+def hamming_distance(p: str, q: str) -> int:
+    assert len(p) == len(q)
+    return sum([1 for a,b in zip(p, q) if a != b])
+
+
+def pattern_count_helper(text, pattern, is_equal):
     i = 0
     n = len(pattern)
     count = 0
@@ -16,13 +30,20 @@ def pattern_count(text, pattern):
         return count
     end = len(text) - n
     while i <= end:
-        if text[i:i+n] == pattern:
+        if is_equal(text[i:i+n], pattern):
             count += 1
         i += 1
     return count
 
+def pattern_count(text, pattern):
+    return pattern_count_helper(text, pattern, lambda x, y: x == y)
 
-def most_frequent_kmer(text, k):
+def pattern_count_approx(text, pattern, d):
+    return pattern_count_helper(text, pattern, 
+            lambda x, y: hamming_distance(x, y) <= d)
+
+
+def most_frequent_kmer(text, k) -> Tuple[List, int]:
     res = set()
     n = 0
     if len(text) < k:
@@ -30,25 +51,78 @@ def most_frequent_kmer(text, k):
     table = defaultdict(int)
     for i in range(0, len(text) - k + 1):
         table[text[i : i + k]] += 1
-    sorted_table = [
-        (v, k) for k, v in sorted(table.items(), key=lambda item: item[1], reverse=True)
-    ]
 
+    sorted_table = [
+        (v, k) for k, v in sorted(table.items(), 
+            key=lambda item: item[1], reverse=True)
+    ]
     if len(sorted_table) == 0:
         return res, n
+
     n = sorted_table[0][0]
     for el in sorted_table:
         if n == el[0]:
             res.add(el[1])
         else:
             break
-    return res, n
+    return list(res), n
 
 def frequency_table(text, k):
     table = defaultdict(int)
     for i in range(0, len(text) - k + 1):
         table[text[i:i + k]] += 1
     return table
+
+"""
+The set of all patterns those Hamming distance is at most d
+"""
+def generate_neighbors(pattern, max_dist):
+    # print("max_dist:", max_dist, type(max_dist))
+    res = {pattern} # the 0 distance is the same as the original
+    n = len(pattern)
+    def build_from_indexes(s, idxs):
+        res = []
+        # print("From idxs: ", idxs)
+        for i in idxs:
+            for g in GENOME_TABLE:
+                if g != s[i]:
+                    res.append(s[:i] + g + s[i+1:])
+        # print("Generated: ", res)
+        return set(res)
+             
+    def foo(indexes, start):
+        res.update(build_from_indexes(pattern, indexes))
+        if len(indexes) >= max_dist:
+            return
+        for i in range(start, n):
+            foo(indexes+[i], i+1)
+
+    foo([], 0)
+    return res
+
+
+
+def frequent_words_with_mismatches(text: str, k: int, d: int) -> List[str]:
+    patterns = []
+    freq_map = defaultdict(int)
+    n = len(text)
+    for i in range(0, n-k+1):
+        pattern = text[i:i+k]
+        # print(i, pattern)
+        neighborhood = generate_neighbors(pattern, d)
+        for neighbor in neighborhood:
+            if neighbor == 'GCACACAGAC':
+                print('GCACACAGAC is neighbor of: ', pattern, "start: ", i)
+            freq_map[neighbor] += 1 
+    max_count = max(freq_map.values()) 
+    print("max_count:", max_count)
+    print(sorted([(k, v) for k, v in freq_map.items() if v >= 8], key=lambda x: x[1]))
+    # print(freq_map)
+    patterns = [key for key, val in freq_map.items() if val == max_count]
+    print('GCACACACAC:', freq_map['GCACACACAC'])
+    print('GCACACAGAC:', freq_map['GCACACAGAC'])
+    return patterns
+
 
 
 def reverse_complement(text, abc_table):
@@ -60,13 +134,6 @@ def reverse_complement(text, abc_table):
             print(f"Could not find {c} as key in abc_table: {abc_table}")
             raise e
     return "".join(res)
-
-GENOME_COMPLEMENT_TABLE = {
-    'a':'t',
-    't':'a',
-    'c':'g',
-    'g':'c',
-}
 
 def reverse_complement_genome(text):
     return reverse_complement(text, abc_table=GENOME_COMPLEMENT_TABLE)
@@ -85,15 +152,26 @@ def pattern_match_indexes(text, pattern) -> List[int]:
         i += 1
     return res
 
+def pattern_match_indexes_approx(text, pattern, d: int) -> Iterable[int]:
+    res = []
+    i = 0
+    n = len(pattern)
+    if n == 0:
+        return res
+    end = len(text) - n
+    while i <= end:
+        if hamming_distance(text[i:i+n], pattern) <= d:
+            res.append(i)
+        i += 1
+    return res
+
+
 
 def position_table(text: str, k: int) -> Dict[str, List[int]]:
     pos_map = {}
     for i in range(len(text) - k + 1):
         pattern = text[i:i + k]
-        try:
-            pos_map[pattern].append(i)
-        except KeyError:
-            pos_map[pattern] = [i]
+        pos_map.setdefault(pattern, []).append(i)
     return pos_map
 
 def is_clump(positions: List[int], k: int, L: int, t: int) -> bool:
@@ -121,21 +199,51 @@ def skew_table(genome):
     n = len(genome)
     for i in range(0, n):
         val = 0
-        if genome[i] == 'g':
+        if genome[i] == 'G':
             val = 1
-        elif genome[i] == 'c':
+        elif genome[i] == 'C':
             val = -1
         table[i+1] = table[i] + val
     return table
+
+def minimum_skew(genome):
+    table = skew_table(genome)
+    min_val = sys.maxsize
+    for i, val in enumerate(table):
+        if val < min_val:
+            min_val = val
+    min_idxs = []
+    for i, val in enumerate(table):
+        if val == min_val:
+            min_idxs.append(i)
+    return min_idxs
 
 
 
 def main(args):
     inp = fileinput.input()
-    text = inp.readline().strip()
-    text = text.lower()
-    for c in skew_table(text):
-        print(c, end=' ')
+    text = inp.readline().strip().upper()
+    k, d = [int(x) for x in inp.readline().split(' ')]
+    # for c in skew_table(text):
+        # print(c, end=' ')
+    # gen_test(args[0], int(args[1]))
+    words = frequent_words_with_mismatches(text, k, d)
+    for n in words:
+        print(n, end=' ')
+    print("")
+
+
+    # debug
+    s = 'GCACACAGAC'
+    print("Neighbors of ", s, " are these:")
+    neighs = generate_neighbors(s, 1)
+    for n in neighs:
+        locs = pattern_match_indexes(text, n)
+        if len(locs) > 0:
+            print("String: ", n)
+            for i in locs:
+                print("found at:", i)
+    # debug end
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- python/test_genome.py
+++ python/test_genome.py
@@ -6,46 +6,62 @@ DATA_FOLDER = "./test_data/"
 
 def test_most_frequent_kmer():
     from genome import most_frequent_kmer
-    assert most_frequent_kmer("aabcccddd", 2) == ({"cc", "dd"}, 2)
-    assert most_frequent_kmer("aabcccddd", 2) == ({"dd", "cc"}, 2)
-    assert most_frequent_kmer("aabcccdddeeee", 2) == ({"ee"}, 3)
+    assert most_frequent_kmer("AABCCCDDD", 2) == ({"CC", "DD"}, 2)
+    assert most_frequent_kmer("AABCCCDDD", 2) == ({"DD", "CC"}, 2)
+    assert most_frequent_kmer("AABCCCDDDEEEE", 2) == ({"EE"}, 3)
 
 def test_pattern_count():
     from genome import pattern_count
-    assert pattern_count("aaaa", "a") == 4
-    assert pattern_count("aaaa", "aaaa") == 1
-    assert pattern_count("aaaa", "aaaaa") == 0
+    assert pattern_count("AAAA", "A") == 4
+    assert pattern_count("AAAA", "AAAA") == 1
+    assert pattern_count("AAAA", "AAAAA") == 0
     assert pattern_count("", "") == 0
-    assert pattern_count("s", "") == 0
-    assert pattern_count("ss", "") == 0
+    assert pattern_count("S", "") == 0
+    assert pattern_count("SS", "") == 0
 
 def test_reverse_complement_genome():
     from genome import reverse_complement_genome
-    assert reverse_complement_genome("atcg") == "cgat"
+    assert reverse_complement_genome("ATCG") == "CGAT"
     assert reverse_complement_genome("") == ""
     with pytest.raises(KeyError):
-        reverse_complement_genome("af") == "at"
+        reverse_complement_genome("AF") == "AT"
     with pytest.raises(KeyError):
-        reverse_complement_genome("A")
-        reverse_complement_genome("T")
-        reverse_complement_genome("C")
-        reverse_complement_genome("G")
+        reverse_complement_genome("a")
+        reverse_complement_genome("t")
+        reverse_complement_genome("c")
+        reverse_complement_genome("g")
 def test_pattern_match_indexes():
     from genome import pattern_match_indexes
-    assert pattern_match_indexes("aaaa", "a") == [0,1,2,3]
-    assert pattern_match_indexes("abcd", "ft") == [] 
-    assert pattern_match_indexes("a", "") == []
-    assert pattern_match_indexes("aa", "") == []
+    assert pattern_match_indexes("AAAA", "A") == [0,1,2,3]
+    assert pattern_match_indexes("ABCD", "FT") == [] 
+    assert pattern_match_indexes("A", "") == []
+    assert pattern_match_indexes("AA", "") == []
     assert pattern_match_indexes("", "") == []
 
 def test_frequency_table():
     from genome import frequency_table
-    assert frequency_table("aabcddabc", 2) == {"aa": 1, "dd": 1, "ab":2, "bc": 2, "cd": 1, "da": 1}
+    assert frequency_table("AABCDDABC", 2) == {"AA": 1, "DD": 1, "AB":2, "BC": 2, "CD": 1, "DA": 1}
 
 def test_find_clumps():
     from genome import find_clumps
     text, nums = [line.strip() for line in open(DATA_FOLDER + "test_find_clumps-input.txt")]
     k, L, t = [int(v) for v in nums.split()]
+    text = text.upper()
     res = find_clumps(text, k, L, t)
     with open(DATA_FOLDER + "test_find_clumps-output.txt") as f:
         assert res == set([v for v in f.readline().strip().split()])
+
+def test_minimum_skew():
+    from genome import minimum_skew
+    genome = "TAAAGACTGCCGAGAGGCCAACACGAGTGCTAGAACGAGGGGCGTAAACGCGGGTCCGAT"
+    expected = {11, 24}
+    assert set(minimum_skew(genome)) == expected
+
+def test_hamming_distance():
+    from genome import hamming_distance
+    assert hamming_distance("hello", "bello") == 1
+    with pytest.raises(Exception):
+        assert hamming_distance("hello", "belloo") == 1
+
+
+
